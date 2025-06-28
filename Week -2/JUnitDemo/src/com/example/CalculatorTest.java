e6aaee44c54935969bdc4f14d5eecf20d1c78a40
Chalamcharla Neha <154206602+ChalamcharlaNeha@users.noreply.github.com>
2025-06-29 00:51:14 +0530

    Add files via upload
    WEEK -2 HandsOn

---
+++ Week -2/JUnitDemo/src/com/example/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(20, calc.multiply(4, 5));
+    }
+}
